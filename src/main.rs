773f332f8cf7790351172a0e1b8a2f5db6f1a956
Cary <carywolff@northwesternmutual.com>
2020-05-17 11:05:44 -0500

    added some comments

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,30 @@
 use std::io;
 
+// need a constant for the numbers the letters equate to
+
+
 fn main() {
 
-    println!("Hello, what's your name?");
+    // ask the user to put in their name
+    println!("=====================================");
+    println!("what is the name you prefer to go by?");
+    println!("=====================================");
 
-    let mut name = String::new(); // ""
+    // setup a var with an empty string
+    let mut name = String::new();
 
+    // get the users name
     io::stdin()
     .read_line(&mut name)
-    .expect("Failed to get your name.");
+    .expect("Failed to get your name."); // if things go wrong with getting their name
 
+    // trim off the \n and split the name into seperate characters
     let split_name = name.trim().chars();
 
+    // loop over the characters
     for letter in split_name {
-        let letr = letter.to_uppercase();
-        let numbr: u8 = letter as u8;
-            println!("{}: {}", letr, numbr);
+        let letr = letter.to_uppercase(); // change it to upper case
+            println!("The letter {} of your name", letr);
     }
 
 }
