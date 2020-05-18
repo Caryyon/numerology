992aa0fea97f83f7ee5fa83bd1edc78c487ee6d5
Cary <carywolff@northwesternmutual.com>
2020-05-18 06:52:27 -0500

    switched to match + tests

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,4 @@
-use std::io;
-
-// need a constant for the numbers the letters equate to
-
-
-fn main() {
-
-    println!("
+/*
      __    _  __   __  __   __  _______  ______    _______  ___      _______  _______  __   __ 
     |  |  | ||  | |  ||  |_|  ||       ||    _ |  |       ||   |    |       ||       ||  | |  |
     |   |_| ||  | |  ||       ||    ___||   | ||  |   _   ||   |    |   _   ||    ___||  |_|  |
@@ -13,29 +6,28 @@ fn main() {
     |  _    ||       ||       ||    ___||    __  ||  |_|  ||   |___ |  |_|  ||   ||  ||_     _|
     | | |   ||       || ||_|| ||   |___ |   |  | ||       ||       ||       ||   |_| |  |   |  
     |_|  |__||_______||_|   |_||_______||___|  |_||_______||_______||_______||_______|  |___|  
-   ");
+*/
 
-    // ask the user to put in their name
+fn convert_name(letter: &str) {
+        match letter {
+            "A" | "J" | "S" => 1,
+            "B" | "K" | "T" => 2,
+            "C" | "L" | "U" => 3,
+            "D" | "M" | "V" => 4,
+            "E" | "N" | "W" => 5,
+            "F" | "O" | "X" => 6,
+            "G" | "P" | "Y" => 7,
+            "H" | "Q" | "Z" => 8,
+            "I" | "R" => 9,
+            &_ => 0 
+        };
+}
 
-    println!("=====================================");
-    println!("what is the name you prefer to go by?");
-    println!("=====================================");
+fn main() {
+    convert_name("A");
+}
 
-    // setup a var with an empty string
-    let mut name = String::new();
-
-    // get the users name
-    io::stdin()
-    .read_line(&mut name)
-    .expect("Failed to get your name."); // if things go wrong with getting their name
-
-    // trim off the \n and split the name into seperate characters
-    let split_name = name.trim().chars();
-
-    // loop over the characters
-    for letter in split_name {
-        let letr = letter.to_uppercase(); // change it to upper case
-            println!("The letter {} of your name", letr);
-    }
-
+#[test]
+fn convert_name_test() {
+    assert_eq!("A", convert_name("A"));
 }
