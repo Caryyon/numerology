a8726b190ebdf20ff428e0b63b323a9805c0f610
Cary <carywolff@northwesternmutual.com>
2020-07-07 13:30:35 -0500

    just keep swimming

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,7 @@ mod utils;
 const NAME: &'static str = "cary";
 fn main() {
     let numb = utils::condense(NAME);
-        println!("end: {}", numb);
-        if (numb < 10) | (numb == 11) | (numb == 22) {
-            let final_number = utils::get_reading(numb);
-            println!("final_number {}", final_number);
-        }
+    println!("end: {}", numb);
+    let res = utils::get_reading(numb);
+    println!("{}", res);
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,24 +1,25 @@
+// TODO: fix this rats nest
 pub fn condense(name: &str) -> u32 {
-    let mut letters_to_numbers = vec![];
+    let mut numbers = vec![];
     let caps = name.to_uppercase();
-    let chars = caps.chars();
-    for letter in chars {
-        let number = convert(&letter.to_string());
-        letters_to_numbers.push(number);
+
+    for letter in caps.chars() {
+        let number = letter_to_number(&letter.to_string());
+        numbers.push(number);
     }
-    println!("{:?}", letters_to_numbers);
-    let ltn_iter = letters_to_numbers.iter();
-    let total: u32 = ltn_iter.sum();
-    println!("{}", total);
-    return total;
+    let ltn_iter = numbers.iter();
+    let num_sum: u32 = ltn_iter.sum();
+    println!("{}", num_sum);
+    return num_sum;
 }
 
+// gives back your final sum number meaning.
 pub fn get_reading(number: u32) -> &'static str {
     let final_number = match number {
         0 => "you are a 0",
         1 => "you are a 1",
         2 => "you are a 2",
-        3 => "you are a 3", 
+        3 => "you are a 3",
         4 => "you are a 4",
         5 => "you are a 5",
         6 => "you are a 6",
@@ -27,30 +28,31 @@ pub fn get_reading(number: u32) -> &'static str {
         9 => "you are a 9",
         11 => "you are a 11",
         22 => "you are a 12",
-        _ =>  "i don't know what you are doing"
+        _ => "i don't know what you are doing",
     };
-    return final_number
+    return final_number;
 }
 
-pub fn convert(letter: &str) -> u32 {
-        let number = match letter {
-            "A" | "J" | "S" => 1,
-            "B" | "K" | "T" => 2,
-            "C" | "L" | "U" => 3,
-            "D" | "M" | "V" => 4,
-            "E" | "N" | "W" => 5,
-            "F" | "O" | "X" => 6,
-            "G" | "P" | "Y" => 7,
-            "H" | "Q" | "Z" => 8,
-            "I" | "R" => 9,
-            &_ => 0
-        };
-        return number
+// pass it a capital letter string and it will return a int
+pub fn letter_to_number(letter: &str) -> u32 {
+    let number = match letter {
+        "A" | "J" | "S" => 1,
+        "B" | "K" | "T" => 2,
+        "C" | "L" | "U" => 3,
+        "D" | "M" | "V" => 4,
+        "E" | "N" | "W" => 5,
+        "F" | "O" | "X" => 6,
+        "G" | "P" | "Y" => 7,
+        "H" | "Q" | "Z" => 8,
+        "I" | "R" => 9,
+        &_ => 0,
+    };
+    return number;
 }
 
 #[test]
-fn convert_test_letter() {
-    assert_eq!(convert("A"), 1);
-    assert_eq!(convert("B"), 2);
-    assert_eq!(convert("C"), 3);
+fn letter_to_number_test() {
+    assert_eq!(letter_to_number("A"), 1);
+    assert_eq!(letter_to_number("B"), 2);
+    assert_eq!(letter_to_number("C"), 3);
 }
