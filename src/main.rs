7f108ab512d509221a99e55c85fd5138f0cabbf1
Cary <carywolff@northwesternmutual.com>
2020-07-16 09:18:31 -0500

    added stdin

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 mod utils;
+use std::io;
 
-const NAME: &'static str = "cary wolff";
 fn main() {
+    let mut name = String::new();
     println!("what is your name?");
-    let name_as_numb = utils::condense(NAME);
+    io::stdin().read_line(&mut name).expect("Didn't get that");
+    let name_as_numb = utils::condense(&name.trim());
     let sum: u32 = name_as_numb.iter().sum();
-    if (sum == 11) | (sum == 12) {
+    if (sum == 11) | (sum == 22) {
         let special_numerology_number = utils::get_reading(sum);
         println!("{}", special_numerology_number);
     } else {
